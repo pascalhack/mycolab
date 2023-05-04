40ed5f05cfbaa10bc29397ada0b0503a3ac6b988
pascalhack <jayzayjz@gmail.com>
2023-05-04 23:01:19 +0800

    Add files via upload

---
+++ volatile-concentration-localux-colab/runaria2.py
@@ -0,0 +1,35 @@
+import os, pickle, subprocess
+
+arialines = None
+colaboptions = None
+downloadcnet = False
+
+if os.path.exists('/content/gdrive/MyDrive/dp/arialist.pkl'):
+  with open('/content/gdrive/MyDrive/dp/arialist.pkl', 'rb') as f:
+      arialines = pickle.load(f)
+
+if os.path.exists('/content/gdrive/MyDrive/dp/colaboptions.pkl'):
+  with open('/content/gdrive/MyDrive/dp/colaboptions.pkl', 'rb') as f:
+      colaboptions = pickle.load(f)
+      downloadcnet = colaboptions["controlnet"]
+      downloadmodels = colaboptions["download_model"]
+
+def subprocessing(execline):
+  try:
+    print("[1;32m" + execline)
+    print('[0m')
+    subprocess.run(execline, shell=True, check=True)
+  except Exception as e:
+      print("Exception: " + str(e))
+
+if arialines:
+  for line in arialines:
+    if not '4x-UltraSharp.pth' in line:
+      ariaexecline = line[2:].replace('\\n",', '').replace('/content/stable-diffusion-webui', '/content/gdrive/MyDrive/dp/volatile-concentration-localux')
+      if 'ControlNet' in ariaexecline:
+         if downloadcnet:
+            subprocessing(ariaexecline)
+      else:
+        if downloadmodels:
+          subprocessing(ariaexecline)
+
